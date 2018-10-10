a11c1e222de96a0d55e44de6887955e04b171a43
marcelino05052000 <marcelino05052000@gmail.com>
2018-10-10 00:11:03 -0300

    adicionada logica pra inimigo faltando ajustes

--- js/script.js
+++ js/script.js
@@ -41,73 +41,97 @@ var config = {
 };
 
 var player;
+var enemie;
 var platforms;
 var cursors;
+var controls = {};
 var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('ceu','img/ceu.png');
-    this.load.image('chao','img/chao.png');
+    this.load.image('ceu','img/Céu.png');
+    this.load.image('chao','img/chao 1.png');
     this.load.image('arvore','img/arvore.png');
-    this.load.image('altar','img/altar.png');
-    this.load.image('espada','img/espada.png');
-    this.load.image('arbusto','img/arbusto.png');
-    this.load.spritesheet('amriel','img/Sprites Amriel NoSword.png', { frameWidth: 73, frameHeight: 120 });
+    this.load.image('folhas','img/Folhas.png');
+    this.load.image('mato','img/Mato.png');
+    this.load.spritesheet('amriel','img/Amriel Sprites Sword.png', { frameWidth: 92.2, frameHeight: 125 });
+    //this.load.spritesheet('dude','img/dude.png'), { frameWidth: 32, frameHeight: 48 };
 }
 
 function create ()
 {
-    this.add.sprite(400,252,'ceu');
+    this.add.sprite(400,300,'ceu'); 
 
     platforms = this.physics.add.staticGroup();
 
-    platforms.create(50,550,'chao').refreshBody();
-    platforms.create(150,550,'chao');
-    platforms.create(250,550,'chao');
-    platforms.create(350,550,'chao');
-    platforms.create(450,550,'chao');
-    platforms.create(550,550,'chao');
-    platforms.create(650,550,'chao');
-    platforms.create(750,550,'chao');
+    platforms.create(0,585,'chao').refreshBody();
+    platforms.create(80,585,'chao');
+    platforms.create(160,585,'chao');
+    platforms.create(240,585,'chao');
+    platforms.create(320,585,'chao');
+    platforms.create(400,585,'chao');
+    platforms.create(480,585,'chao');
+    platforms.create(560,585,'chao');
+    platforms.create(640,585,'chao');
+    platforms.create(720,585,'chao');
+    platforms.create(800,585,'chao');
 
+    this.add.sprite(300,460,'arvore');
+    this.add.sprite(660,460,'arvore');
 
-    this.add.sprite(140,354,'arvore');
-    this.add.sprite(660,354,'arvore');
+    this.add.sprite(340,560,'folhas');
+    this.add.sprite(700,560,'folhas');
 
-    this.add.sprite(400,430,'altar');
+    this.add.sprite(180,565,'mato');
+    this.add.sprite(570,565,'mato');
 
-    this.add.sprite(404,360,'espada');
-
-    this.add.sprite(60,480,'arbusto');
-    this.add.sprite(730,480,'arbusto');
-
-    player = this.physics.add.sprite(100, 400, 'amriel');
+    //enemie = this.physics.add.sprite(300, 500, 'dude');
+    player = this.physics.add.sprite(100, 500, 'amriel');
 
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
     this.anims.create({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('amriel', { start: 0, end: 3 }),
+        frames: this.anims.generateFrameNumbers('amriel', { start: 2, end: 5 }),
         frameRate: 10,
         repeat: -1
     });
 
     this.anims.create({
         key: 'turn',
-        frames: [ { key: 'amriel', frame: 4 } ],
+        frames: [ { key: 'amriel', frame: 6 } ],
         frameRate: 20
     });
 
     this.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('amriel', { start: 4, end: 7 }),
+        frames: this.anims.generateFrameNumbers('amriel', { start: 6, end: 9 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'right-sword',
+        frames: this.anims.generateFrameNumbers('amriel', { start: 10, end: 10 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'left-sword',
+        frames: this.anims.generateFrameNumbers('amriel', { start: 1, end: 1 }),
         frameRate: 10,
         repeat: -1
     });
 
     cursors = this.input.keyboard.createCursorKeys();
+    /*controls = {
+        right: this.input.keyboard.addKey(Phaser.keyboard.D),
+        left: this.input.keyboard.addKey(Phaser.keyboard.A),
+        up: this.input.keyboard.addKey(Phaser.keyboard.W),
+        sword: this.input.keyboard.addKey(Phaser.keyboard.SPACE)
+    }*/
 
     this.physics.add.collider(player, platforms);
 }
@@ -137,4 +161,17 @@ function update ()
     {
         player.setVelocityY(-330);
     }
+/*
+    if (controls.sword.isDown && controls.right.isDown)
+    {
+        player.setVelocityX(160);
+        player.anims.play('right-sword');
+    }
+
+    if (controls.sword.isDown && controls.left.isDown)
+    {
+        player.setVelocityX(-160);
+        player.anims.play('left-sword');
+    }*/
+
 }
